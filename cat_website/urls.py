850899126fa294cd0480e8cee74858ffdc1fd5f5
naznin <mitu.bauet@gmail.com>
2025-08-20 20:20:33 +0600

    feat: static files add

--- cat_website/urls.py
+++ cat_website/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
 ]
+# ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# This block is crucial for serving media in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
