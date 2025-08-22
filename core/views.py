3db1279a6be69b4e25bc1175c56b07b94c58d895
naznin <mitu.bauet@gmail.com>
2025-08-22 19:56:10 +0600

    feat: cat food page add

--- core/views.py
+++ core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "core/index.html", context={})
+
+def cat_food(request):
+    cat_food_one = "Shutki"
+    cat_food_two = "Purina"
+    cat_food_three = "Pro Plan"
+
+    cat_food = [cat_food_one, cat_food_two, cat_food_three]
+    return render(request, "core/cat_food.html", context={'cat_food': cat_food_one,})
